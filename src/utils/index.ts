300a0895eeff6e07abfe9c8f9bbc9185467689e2
方啸宇 <maskedfang@gmail.com>
2021-12-27 12:32:33 +0800

    chore any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
 
 // 在一个函数里，改变传入的对象本身是不好的 (immutable)
 // TODO
@@ -7,24 +6,24 @@ import { useState, useEffect } from 'react';
 export const cleanObject = (object: object) => {
   // TODO
   // Object.assign({}, object)
-  const result = { ...object }
-  Object.keys(result).forEach(key => {
-    const value = result[key]
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
     // TODO
     // 包含了undefined null 0
     if (!value) {
-      delete result[key]
+      delete result[key];
     }
-  })
-  return result
-}
+  });
+  return result;
+};
 
 // 自定义hook 必须以use开头
 export const useMount = (callback: () => void) => {
   useEffect(() => {
-    callback()
-  }, [])
-}
+    callback();
+  }, []);
+};
 
 // const debounce = (func, delay) => {
 //   let timeout
@@ -37,17 +36,23 @@ export const useMount = (callback: () => void) => {
 //     }, delay)
 //   }
 // }
-export const useDebounce = (value: any, delay?: number) => {
-  const [debuouncedValue, setDebouncedValue] = useState(value)
+
+// unknown不能赋给别的变量，也不能从unknown上读取任何方法
+// 我们想让这个函数的返回值 为 value的类型，而不是unknown类型
+// 所以考虑后面用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
+  const [debuouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     // 每次在value变化以后，设置一个定时器
-    const timeout = setTimeout(() => setDebouncedValue(value), delay)
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
     // 每次在上一个useEffect处理完以后再运行
-    return () => clearTimeout(timeout)
-  }, [value, delay])
-  return debuouncedValue
-}
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debuouncedValue;
+};
 
-export const isFalsy = (value: any) => (value === 0 ? false : !value)
+export const isFalsy: (value: unknown) => boolean = (value) => {
+  return value === 0 ? false : !value;
+};
 // TODO
 //!!的意思就是把一个值转换成布尔值

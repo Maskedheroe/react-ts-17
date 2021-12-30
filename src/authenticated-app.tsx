80d8c4c14ac148796e2c453a605038c4de40da04
方啸宇 <maskedfang@gmail.com>
2021-12-30 10:18:03 +0800

    用emotion封装了一个Row组件

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import { ProjectListScreen } from 'screens/project-list'
-import { useAuth } from './context/auth-context';
-import { Button } from 'antd';
+import React from "react";
+import { ProjectListScreen } from "screens/project-list";
+import { useAuth } from "./context/auth-context";
+import { Button } from "antd";
+import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 
 export const AuthenticatedApp = () => {
-  const { logout } = useAuth()
+  const { logout } = useAuth();
   return (
-    <div>
-      <Button type='primary' onClick={logout}>登出</Button>
-      <ProjectListScreen></ProjectListScreen>
-    </div>
-  )
-}
+    <Container>
+      <Header>
+        <HeaderLeft gap={true} between={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button type="primary" onClick={logout}>
+            登出
+          </Button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen></ProjectListScreen>
+      </Main>
+    </Container>
+  );
+};
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem calc(100vh - 6rem);
+  height: 100vh;
+`;
+
+const Header = styled(Row)``;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;

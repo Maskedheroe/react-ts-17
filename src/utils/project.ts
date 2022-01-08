1d55c8e41fbc74ba0e6587a2ead2435b0a089761
方啸宇 <maskedfang@gmail.com>
2022-01-08 13:57:01 +0800

    useCallback和useMemo的用法就是当非基本类型做依赖时,把它们包一层

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,49 +1,54 @@
-
-import { useEffect } from 'react';
-import { useAsync } from './use-async';
-import { cleanObject } from './index';
-import { Project } from 'screens/project-list/list'
-import { useHttp } from 'utils/http';
+import { useEffect, useCallback } from "react";
+import { useAsync } from "./use-async";
+import { cleanObject } from "./index";
+import { Project } from "screens/project-list/list";
+import { useHttp } from "utils/http";
 
 export const useProjects = (param?: Partial<Project>) => {
-  const client = useHttp()
-  const { run, ...result } = useAsync<Project[]>()
-  const fetchProjects = () => client('projects', {data: cleanObject(param || {})})
+  const client = useHttp();
+  const { run, ...result } = useAsync<Project[]>();
+  const fetchProjects = useCallback(
+    () => client("projects", { data: cleanObject(param || {}) }),
+    [client, param]
+  );
   useEffect(() => {
     run(fetchProjects(), {
-      retry: fetchProjects
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [param]);
+      retry: fetchProjects,
+    });
+  }, [param, run, fetchProjects]);
 
-  return result
-}
+  return result;
+};
 
 export const useEditProject = () => {
-  const { run, ...asyncResult } = useAsync()
-  const client = useHttp()
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
   const mutate = (params: Partial<Project>) => {
-    return run(client(`projects/${params.id}`, {
-      data: params,
-      method: 'PATCH'
-    }))
-  }
+    return run(
+      client(`projects/${params.id}`, {
+        data: params,
+        method: "PATCH",
+      })
+    );
+  };
   return {
     mutate,
-    ...asyncResult
-  }
-}
+    ...asyncResult,
+  };
+};
 export const useAddProject = () => {
-  const { run, ...asyncResult } = useAsync()
-  const client = useHttp()
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
   const mutate = (params: Partial<Project>) => {
-    return run(client(`projects/${params.id}`, {
-      data: params,
-      method: 'POST'
-    }))
-  }
+    return run(
+      client(`projects/${params.id}`, {
+        data: params,
+        method: "POST",
+      })
+    );
+  };
   return {
     mutate,
-    ...asyncResult
-  }
-}
+    ...asyncResult,
+  };
+};
--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,78 +1,92 @@
-import { useState } from 'react';
-import { useMountedRef } from 'utils';
-
+import { useCallback, useState } from "react";
+import { useMountedRef } from "utils";
 
 interface State<D> {
   error: Error | null;
-  data: D | null
-  stat: 'idle' | 'loading' | 'error' | 'success'
+  data: D | null;
+  stat: "idle" | "loading" | "error" | "success";
 }
 
 const defaultInitialState: State<null> = {
-  stat: 'idle',
+  stat: "idle",
   data: null,
-  error: null
-}
+  error: null,
+};
 
 const defaultConfig = {
-  throwOnError: false
-}
+  throwOnError: false,
+};
 
-export const useAsync = <D>(initialState?: State<D>, initialConfig?: typeof defaultConfig) => {
-  const config = {...defaultConfig, initialConfig}
+export const useAsync = <D>(
+  initialState?: State<D>,
+  initialConfig?: typeof defaultConfig
+) => {
+  const config = { ...defaultConfig, initialConfig };
   const [state, setState] = useState<State<D>>({
     ...defaultInitialState,
-    ...initialState
-  })
-  const [retry, setRetry] = useState(() => () => {
-  }) // useState直接传入函数的含义是惰性初始化。所以要传入两层函数
+    ...initialState,
+  });
+  const [retry, setRetry] = useState(() => () => {}); // useState直接传入函数的含义是惰性初始化。所以要传入两层函数
 
-  const mountedRef = useMountedRef()
+  const mountedRef = useMountedRef();
 
-  const setData = (data: D) => setState({
-    data,
-    stat: 'success',
-    error: null
-  })
-  const setError = (error: Error) => setState({
-    error,
-    stat: 'error',
-    data: null
-  })
+  const setData = useCallback(
+    (data: D) =>
+      setState({
+        data,
+        stat: "success",
+        error: null,
+      }),
+    []
+  );
+  const setError = useCallback(
+    (error: Error) =>
+      setState({
+        error,
+        stat: "error",
+        data: null,
+      }),
+    []
+  );
 
   // run用来触发异步请求
-  const run = (promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
-    if (!promise || !promise.then) {
-      throw new Error('请传入 Promise 类型数据')
-    }
-    setRetry(() => () => {
-      if (runConfig?.retry) {
-        run(runConfig.retry(), runConfig)
+  const run = useCallback(
+    (promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
+      if (!promise || !promise.then) {
+        throw new Error("请传入 Promise 类型数据");
       }
-    })
-    setState({...state, stat: 'loading'})
-    return promise.then(data => {
-      mountedRef.current && setData(data)
-      return data
-    }).catch(error => {
-      // catch 会处理异常，如果不主动抛出外部接收不到异常
-      setError(error)
-      if (config.throwOnError) {
-        return Promise.reject(error)
-      }
-      return Promise.reject(error)
-    })
-  }
+      setRetry(() => () => {
+        if (runConfig?.retry) {
+          run(runConfig.retry(), runConfig);
+        }
+      });
+      setState(prevState => ({ ...prevState, stat: "loading" }))
+      return promise
+        .then((data) => {
+          mountedRef.current && setData(data);
+          return data;
+        })
+        .catch((error) => {
+          // catch 会处理异常，如果不主动抛出外部接收不到异常
+          setError(error);
+          if (config.throwOnError) {
+            return Promise.reject(error);
+          }
+          return Promise.reject(error);
+        });
+    },
+    [config.throwOnError, mountedRef, setData, setError, setState]
+  );
+
   return {
-    isIdle: state.stat === 'idle',
-    isLoading: state.stat === 'loading',
-    isError: state.stat === 'error',
-    isSuccess: state.stat === 'success',
+    isIdle: state.stat === "idle",
+    isLoading: state.stat === "loading",
+    isError: state.stat === "error",
+    isSuccess: state.stat === "success",
     run,
     setData,
     setError,
     retry,
-    ...state
-  }
-}
-
+    ...state,
+  };
+};
